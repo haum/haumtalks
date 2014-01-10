c72bc240b9f313457a92cfccaed36fcaaec54adb
Mathieu (matael) Gaborit <mat.gaborit@gmx.com>
2014-01-10 19:28:14 +0100

    Ecriture du form pour s'inscrire en temps qu'auditeur

--- talksplanning/forms.py
+++ talksplanning/forms.py
@@ -43,3 +43,21 @@ class TalkProposalForm(forms.ModelForm):
         HackerBatch(orateur=True, batch=batch, hacker=speaker).save()
 
         return talk
+
+class ListenerForm(forms.ModelForm):
+    """ Form to register as a litener """
+
+    class Meta:
+        model = Hacker
+
+    def save(self, batch):
+
+        listener = Hacker.objects.get_or_create(
+            pseudo=self.cleaned_data['pseudo'],
+            mail=self.cleaned_data['mail'],
+            haum=self.cleaned_data.get('haum') # can be blank
+        )[0] # indice comme pour le TalkProposalForm
+
+        HackerBatch(batch=batch, hacker=listener).save()
+
+        return listener
