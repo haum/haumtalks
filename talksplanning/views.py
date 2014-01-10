f2516c5c4dd7663c70e2c29d748b6fb052810d9e
Mathieu (matael) Gaborit <mat.gaborit@gmx.com>
2014-01-10 19:05:05 +0100

    Implémentation de la vue d'ajout de talk

--- talksplanning/views.py
+++ talksplanning/views.py
@@ -1,6 +1,10 @@
 # -*- encoding:utf8 -*-
 from django.shortcuts import render_to_response, get_object_or_404
 
+from django.template import RequestContext
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+
 from talksplanning.models import Batch, Talk, Hacker
 from talksplanning.forms import TalkProposalForm
 
@@ -31,8 +35,17 @@ def talk_form(request, batch_id):
     """ Formulaire inscription à un talk """
 
     batch = get_object_or_404(Batch, pk=batch_id)
-    form = TalkProposalForm()
-    return render_to_response('talk_form.html', {'batch':batch, 'form': form})
+
+    if request.method == 'POST':
+        f = TalkProposalForm(request.POST)
+        if not f.is_valid():
+            return render_to_response('talk_form.html', {'batch':batch, 'form': f}, context_instance=RequestContext(request))
+        else:
+            talks = f.save(batch)
+            return HttpResponseRedirect(reverse('batch_detail', args=(batch.id,)))
+    else:
+        form = TalkProposalForm()
+        return render_to_response('talk_form.html', {'batch':batch, 'form': form}, context_instance=RequestContext(request))
 
 
 def add_talk(request, batch_id):
