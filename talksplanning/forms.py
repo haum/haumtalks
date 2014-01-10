6c80581fa796fda09b6354220ff622be50c0b028
Mathieu (matael) Gaborit <mat.gaborit@gmx.com>
2014-01-10 19:02:53 +0100

    Override save() method for TalkProposalTalk

--- talksplanning/forms.py
+++ talksplanning/forms.py
@@ -3,7 +3,7 @@
 from django import forms
 from django.forms.models import modelformset_factory, inlineformset_factory
 
-from talksplanning.models import Talk, Hacker
+from talksplanning.models import Talk, Hacker, HackerBatch
 
 class TalkProposalForm(forms.ModelForm):
     """ Form to propose a talk """
@@ -17,4 +17,30 @@ class TalkProposalForm(forms.ModelForm):
     hacker_haum = forms.BooleanField()
     
 
+    def save(self, batch):
 
+        # shorter is better
+        cd = self.cleaned_data
+
+        # == Hacker creation ==
+        # 0 indice to get the Hacker instance
+        speaker = Hacker.objects.get_or_create(
+            pseudo = cd['hacker_name'],
+            mail = cd['hacker_mail'],
+            haum = cd['hacker_haum']
+        )[0]
+        batch = batch
+
+        # == Talk ==
+        talk = Talk()
+        talk.titre = cd['titre']
+        talk.description = cd['description']
+        talk.url = cd.get('url') # may be None
+        talk.speaker = speaker
+        talk.batch = batch
+        talk.save()
+
+        # == Link Hacker <-> Batch ==
+        HackerBatch(orateur=True, batch=batch, hacker=speaker).save()
+
+        return talk
