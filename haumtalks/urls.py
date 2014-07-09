94bb55edf423b82fca97d228a223ebe9f3f817ea
Mathieu (matael) Gaborit <mat.gaborit@gmx.com>
2014-07-09 16:02:21 +0200

    Changement des URLs pour la nouvelle archi

--- haumtalks/urls.py
+++ haumtalks/urls.py
@@ -3,8 +3,12 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
+# to name index
+import talksplanning.views
+
 urlpatterns = patterns('',
-    url(r'^', include('talksplanning.urls')),
+    url(r'^/?$', talksplanning.views.home, name="index"),
+    url(r'^', include('talksplanning.urls', namespace="talksplanning")),
 
     url(r'^admin/', include(admin.site.urls)),
 )
